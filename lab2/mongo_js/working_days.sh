1d1c61356aa6f5b669698ea94f5c72ac4ed9b3ca
Matteo <nisimatteo834@gmail.com>
2018-01-17 11:29:47 +0100

    Weekends and WD

--- lab2/mongo_js/working_days.sh
+++ lab2/mongo_js/working_days.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo 'Working on Enjoy Weekends...'
+echo 'Working on Enjoy WorkingD...'
 mongo bigdatadb.polito.it/carsharing --ssl --sslAllowInvalidCertificates -u ictts -p 'Ictts16!' --authenticationDatabase carsharing enjoy_wd.js > enjoy_wd.csv 
 echo 'Done'
 
-echo 'Working on Car2Go Weekends...'
+echo 'Working on Car2Go WorkingD...'
 mongo bigdatadb.polito.it/carsharing --ssl --sslAllowInvalidCertificates -u ictts -p 'Ictts16!' --authenticationDatabase carsharing car2go_wd.js > car2go_wd.csv
 echo 'Done'
 
