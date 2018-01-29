0c6802bca1556fb75241319134bfa72b0e9725cb
NvdYmn <navidyamini@gmail.com>
2018-01-29 20:03:34 +0100

    Add files via upload

---
+++ lab1_2_1/alternative.js
@@ -0,0 +1,20 @@
+var start_date = new Date("2017-10-01")  
+var start = start_date.getTime()/1000+1*60*60 - 24*60*60   
+var end = start + 24*60*60   
+db.PermanentBookings.aggregate([    
+{$match:    
+    {   
+        "walking.duration":{$ne:-1},   
+        $or:[   
+            {city:"Torino"},   
+            {city:"Madrid"},             
+            {city:"New York City"}            
+             ]   
+     }  
+ },    
+{$group:{    
+    _id:"$city",    
+    bookings:{$sum:1}    
+}},    
+{$sort:    
+    { _id:1}}])   
